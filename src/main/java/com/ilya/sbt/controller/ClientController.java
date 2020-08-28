8e34dbbb99b3289881d487696887555535ffa51f
ilya <ilyaman5@gmail.com>
2020-08-28 13:49:06 +0700

    rename main controller to client controller

--- src/main/java/com/ilya/sbt/controller/ClientController.java
+++ src/main/java/com/ilya/sbt/controller/ClientController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/clients/")
-public class MainController {
+public class ClientController {
     @Autowired
     private ClientServiceImpl clientService;
 
