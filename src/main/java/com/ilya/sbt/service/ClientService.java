3875c9cbddda3b9f5c988adafcab2cf6d29e48e3
ilya <ilyaman5@gmail.com>
2020-08-28 11:37:55 +0700

    refactor client service interface

--- src/main/java/com/ilya/sbt/service/ClientService.java
+++ src/main/java/com/ilya/sbt/service/ClientService.java
@@ -1,11 +1,10 @@
 package com.ilya.sbt.service;
 
-import com.ilya.sbt.domain.Client;
 import com.ilya.sbt.dto.ClientDTO;
 
 import java.util.List;
 
 public interface ClientService {
-    public List<ClientDTO> clientList();
-    public ClientDTO getClient(Long id);
+    List<ClientDTO> clientList();
+    ClientDTO getClient(Long id);
 }
